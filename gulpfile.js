67f4e84893aec6e1a4566a318d2eb70bcd02b21d
EA <icyrealm@gmail.com>
2020-11-28 19:45:17 -0500

    added gulp process

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const { src, dest, series, parallel } = require('gulp');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const del = require('del');
+const minifyHTML = require('gulp-minify-html');
+
+const clean = () => del([ 'docs' ]);
+
+function html(){
+    return src('src/**/*.{htm,html}')
+        .pipe(minifyHTML())
+        .pipe(dest('docs/'));
+}
+
+function imgs(){
+    return src('src/images/**/*')
+        .pipe(dest('docs/images'));
+}
+
+function styles() {
+    return src('src/lib/style.css', { allowEmpty: true })
+        .pipe(cleanCSS())
+        .pipe(rename({
+            basename: 'main',
+            suffix: '.min'
+        }))
+        .pipe(dest('docs/lib/styles'))
+}
+
+function scripts() {
+    return src('src/lib/*.js', { allowEmpty: true })
+        .pipe(minify({noSource: true}))
+        .pipe(dest('docs/lib/js'))
+}
+
+const build = series(clean, parallel(html, imgs, styles, scripts));
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.clean = clean;
+exports.blean = build;
+
+exports.default = build;
